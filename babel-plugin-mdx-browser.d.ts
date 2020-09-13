67d5e8e314fb53561dbeeaddc77378d5c6566d43
JunichiSugiura <jun.sugiura.jp@gmail.com>
2020-09-13 15:55:39 +0200

    remove unnecessary files

--- babel-plugin-mdx-browser.d.ts
+++
@@ -1,7 +0,0 @@
-export default function BabelPluginMdxBrowser(): {
-  visitor: {
-    ImportDeclaration(path: any): void
-    VariableDeclaration(path: any): void
-  }
-}
-//# sourceMappingURL=babel-plugin-mdx-browser.d.ts.map
