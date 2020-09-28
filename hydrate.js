e50b61f5255d71d672b8c42af92074f1f40b0e7b
Luis Alvarez <luis@vercel.com>
2020-09-28 00:05:38 -0500

    Make hydrate fully IE11 compatible

--- hydrate.js
+++ hydrate.js
@@ -1,16 +1,12 @@
 const React = require('react')
 const { mdx, MDXProvider } = require('@mdx-js/react')
 
-// The code here is IE 11 compatible
+// The code here is IE 11 compatible, and only works if Next.js polyfills are included
 module.exports = function hydrate(source, props, options) {
   const scope = (options && options.scope) || {}
-  const getContent = new Function(
-    'mdx',
-    ...Object.keys(scope),
-    `${source}
-return MDXContent;`
-  )
-  const MDXContent = getContent(mdx, ...Object.values(scope))
+  const args = ['mdx'].concat(Object.keys(scope), source + 'return MDXContent;')
+  const getContent = Function.apply(null, args)
+  const MDXContent = getContent.apply(null, [mdx].concat(Object.values(scope)))
 
   return React.createElement(
     MDXProvider,
