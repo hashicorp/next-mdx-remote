a1a6a566c2c133d8539e9dd49f074b5576b1e5d0
Luis Alvarez <luis@vercel.com>
2020-11-15 16:36:35 -0500

    Updated hydrate to be component

--- hydrate.js
+++ hydrate.js
@@ -5,15 +5,18 @@ const mdxReact = require('@mdx-js/react')
 const mdx = mdxReact.mdx
 const MDXProvider = mdxReact.MDXProvider
 
-module.exports = function hydrate(source, props, options) {
-  const scope = (options && options.scope) || {}
-  const args = ['mdx'].concat(Object.keys(scope), source + 'return MDXContent;')
+function MdxRemote(props) {
+  const source = props.source + 'return MDXContent;'
+  const scope = props.scope || {}
+  const args = ['mdx'].concat(Object.keys(scope), source)
   const getContent = Function.apply(null, args)
   const MDXContent = getContent.apply(null, [mdx].concat(Object.values(scope)))
 
   return React.createElement(
     MDXProvider,
-    { components: props && props.components },
+    { components: props.components },
     React.createElement(MDXContent, props)
   )
 }
+
+module.exports = React.memo(MdxRemote)
