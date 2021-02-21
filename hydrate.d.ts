79d438e8cd43eb141afda8c4d4d0e9de54751a7d
alattalatta <urty5656@gmail.com>
2021-02-21 19:53:48 +0900

    restore hydrate with deprecation

---
+++ hydrate.d.ts
@@ -0,0 +1,6 @@
+import useHydrate from './useHydrate'
+
+/** @deprecated use `next-mdx-remote/useHydrate` instead. */
+const hydrate = useHydrate
+
+export default hydrate
--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,13 @@
+import hydrate from './hydrate'
 import renderToString from './render-to-string'
 import useHydrate from './useHydrate'
 
 declare module 'next-mdx-remote/render-to-string' {
   export default renderToString
+}
+
+declare module 'next-mdx-remote/hydrate' {
+  export default hydrate
 }
 
 declare module 'next-mdx-remote/useHydrate' {
