28d667f00e4c7d028f3fb597e58b205db0cffa10
Jake Jarvis <jake@jarv.is>
2022-03-25 10:46:43 -0400

    fix: Can't resolve 'react/jsx-runtime.js' (#250)

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ export default [
       dir: './dist',
       format: 'es',
     },
-    external: ['react', '@mdx-js/react', 'react/jsx-runtime.js'],
+    external: ['react', '@mdx-js/react', 'react/jsx-runtime'],
     plugins: [
       ts({
         tsconfig: './tsconfig.json',
