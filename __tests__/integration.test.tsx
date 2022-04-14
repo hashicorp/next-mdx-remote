714da1b0f0e07e153e18eaf9f4b25900d7e4290f
Krystian Paszkiewicz <krystian.paszkiewicz@gmail.com>
2022-04-14 21:52:42 +0100

    chore: update react dependency and fix tests

--- __tests__/integration.test.tsx
+++ __tests__/integration.test.tsx
@@ -22,9 +22,13 @@ describe('hydration', () => {
     const $ = cheerio.load(result)
 
     // server renders correctly
-    expect($('#__next').html()).toContain(`<h1>foo</h1><h1>Headline</h1>
-<p>hello <!-- -->jeff</p><button>Count: <!-- -->0</button>
-<p class=\"context\">Context value: \"<!-- -->foo<!-- -->\"</p>
+    expect(
+      $('#__next')
+        .html()
+        .replace(/<!-- -->/g, '')
+    ).toContain(`<h1>foo</h1><h1>Headline</h1>
+<p>hello jeff</p><button>Count: 0</button>
+<p class=\"context\">Context value: \"foo\"</p>
 <p>Some <strong class=\"custom-strong\">markdown</strong> content</p>
 <div class=\"alert alert-warning g-type-body\"><p>Alert</p></div>
 <div>I am a dynamic component.</div>`)
