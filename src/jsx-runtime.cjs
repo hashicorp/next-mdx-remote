052bdcaf55a3edd34f4cd9680fb4b66fbcb8dd8d
imtsuki <me@qjx.app>
2022-12-22 18:02:03 -0500

    fix: export `jsx-dev-runtime` in development mode
    From `@mdx-js/mdx` version 2.2.0 onwards, the compiled source uses
    `jdxDEV` from `react/jsx-dev-runtime` in development mode instead.
    This patch determines whether we should export `jsx-dev-runtime`
    by checking `process.env.NODE_ENV`. This environment variable is
    always present in Next.js both in browser and server environment.
    `@mdx-js/*` are bumped to `^2.2.1` because this change is incompatible
    with version 2.1.5 or older.
    
    Signed-off-by: imtsuki <me@qjx.app>

--- src/jsx-runtime.cjs
+++ src/jsx-runtime.cjs
@@ -3,6 +3,8 @@
  *
  * Inspired by the approach here: https://github.com/contentlayerdev/contentlayer/blob/main/packages/next-contentlayer/src/hooks/jsx-runtime.cjs
  */
-const jsxRuntime = require('react/jsx-runtime')
-
-module.exports.jsxRuntime = jsxRuntime
+if (process.env.NODE_ENV === 'development') {
+  module.exports.jsxRuntime = require('react/jsx-dev-runtime')
+} else {
+  module.exports.jsxRuntime = require('react/jsx-runtime')
+}
