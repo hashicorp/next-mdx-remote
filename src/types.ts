67a221fa59418af1289ea360ba42a2cdc3c2e5c1
Dylan Staley <88163+dstaley@users.noreply.github.com>
2024-05-22 10:42:53 -0700

    `next-mdx-remote` v5 (#448)
    * feat: update to MDX v3
    
    * feat: remove rollup
    
    * feat: replace jest with vitest
    
    * ci: run tests on all PRs
    
    * fix: move @babel/code-frame to deps
    
    * fix: add extension to polyfill import
    
    * fix: add extension to type imports
    
    * feat: restore useDynamicImport
    
    * feat: add stable release workflow
    
    * docs: update README
    
    * feat: allow react v19
    
    * chore: update changeset

--- src/types.ts
+++ src/types.ts
@@ -14,7 +14,9 @@ export interface SerializeOptions {
    * These options are passed to the MDX compiler.
    * See [the MDX docs.](https://github.com/mdx-js/mdx/blob/master/packages/mdx/index.js).
    */
-  mdxOptions?: Omit<CompileOptions, 'outputFormat' | 'providerImportSource'>
+  mdxOptions?: Omit<CompileOptions, 'outputFormat' | 'providerImportSource'> & {
+    useDynamicImport?: boolean
+  }
   /**
    * Indicate whether or not frontmatter should be parsed out of the MDX. Defaults to false
    */
