e4a13a25eb304b715d7b39f6e174eae9bc66f30a
Nels Andereck <nels.andereck@hashicorp.com>
2026-01-07 08:35:54 -0800

    disallow dynamic imports

--- src/serialize.ts
+++ src/serialize.ts
@@ -14,62 +14,61 @@ import { removeImportsExportsPlugin } from './plugins/remove-imports-exports.js'
 import type { MDXRemoteSerializeResult, SerializeOptions } from './types.js'
 
 function getCompileOptions(
-  mdxOptions: SerializeOptions['mdxOptions'] = {},
-  rsc: boolean = false
+	mdxOptions: SerializeOptions['mdxOptions'] = {},
+	rsc: boolean = false
 ): CompileOptions {
-  const areImportsEnabled = mdxOptions.useDynamicImport ?? false
-  // don't modify the original object when adding our own plugin
-  // this allows code to reuse the same options object
-  const remarkPlugins = [
-    ...(mdxOptions.remarkPlugins || []),
-    ...(areImportsEnabled ? [] : [removeImportsExportsPlugin]),
-  ]
+	// don't modify the original object when adding our own plugin
+	// this allows code to reuse the same options object
+	const remarkPlugins = [
+		...(mdxOptions.remarkPlugins || []),
+		...([removeImportsExportsPlugin]),
+	]
 
-  return {
-    ...mdxOptions,
-    remarkPlugins,
-    outputFormat: 'function-body',
-    // Disable the importSource option for RSC to ensure there's no `useMDXComponents` implemented.
-    providerImportSource: rsc ? undefined : '@mdx-js/react',
-    development: process.env.NODE_ENV !== 'production',
-  }
+	return {
+		...mdxOptions,
+		remarkPlugins,
+		outputFormat: 'function-body',
+		// Disable the importSource option for RSC to ensure there's no `useMDXComponents` implemented.
+		providerImportSource: rsc ? undefined : '@mdx-js/react',
+		development: process.env.NODE_ENV !== 'production',
+	}
 }
 
 /**
  * Parses and compiles the provided MDX string. Returns a result which can be passed into <MDXRemote /> to be rendered.
  */
 export async function serialize<
-  TScope = Record<string, unknown>,
-  TFrontmatter = Record<string, unknown>
+	TScope = Record<string, unknown>,
+	TFrontmatter = Record<string, unknown>
 >(
-  source: VFileCompatible,
-  {
-    scope = {},
-    mdxOptions = {},
-    parseFrontmatter = false,
-  }: SerializeOptions = {},
-  rsc: boolean = false
+	source: VFileCompatible,
+	{
+		scope = {},
+		mdxOptions = {},
+		parseFrontmatter = false,
+	}: SerializeOptions = {},
+	rsc: boolean = false
 ): Promise<MDXRemoteSerializeResult<TScope, TFrontmatter>> {
-  const vfile = new VFile(source)
+	const vfile = new VFile(source)
 
-  // makes frontmatter available via vfile.data.matter
-  if (parseFrontmatter) {
-    matter(vfile, { strip: true })
-  }
+	// makes frontmatter available via vfile.data.matter
+	if (parseFrontmatter) {
+		matter(vfile, { strip: true })
+	}
 
-  let compiledMdx: VFile
+	let compiledMdx: VFile
 
-  try {
-    compiledMdx = await compile(vfile, getCompileOptions(mdxOptions, rsc))
-  } catch (error: any) {
-    throw createFormattedMDXError(error, String(vfile))
-  }
+	try {
+		compiledMdx = await compile(vfile, getCompileOptions(mdxOptions, rsc))
+	} catch (error: any) {
+		throw createFormattedMDXError(error, String(vfile))
+	}
 
-  let compiledSource = String(compiledMdx)
+	let compiledSource = String(compiledMdx)
 
-  return {
-    compiledSource,
-    frontmatter: (vfile.data.matter ?? {}) as TFrontmatter,
-    scope: scope as TScope,
-  }
+	return {
+		compiledSource,
+		frontmatter: (vfile.data.matter ?? {}) as TFrontmatter,
+		scope: scope as TScope,
+	}
 }
